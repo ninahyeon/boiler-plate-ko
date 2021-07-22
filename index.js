8ad53a4cbd2d5eb2fe99b8e9f90b890216fa4020
ninahyeon <ninahyeon@gmail.com>
2021-07-22 23:30:18 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+//application//x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ninaKim:1111@boilerplate.t1z32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세요')
+  res.send('히히 나발이 노드 리액트!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+app.post('/register',(req,res)=>{
+  //회원가입 시 필요한 정보를 client에서 가져오면
+  //이를 DB에 넣음
+  const user = new User(req.body)
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })   //몽고db 메서드
 })
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}!`))
